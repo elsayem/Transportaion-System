0cd14456a913402cdd4a7c0d2b00ca4b0449d157
asaiem <eng.abdulrahman.alsaiem@gmail.com>
2024-02-11 22:32:04 +0200

    Addin Constants

--- Core/Constants/Errors.cs
+++ Core/Constants/Errors.cs
@@ -9,5 +9,7 @@ namespace TransportReservationSystem.Core.Constants
     public class Errors
     {
         public static string PasswordInvalid { get; set; } = "Password must at least 8 character and numbers ..!!";
+        public static string EmailRequiredMSG { get; set; } = "Email Fiel Must Be Required..!!";
+        public static string PasswordRequiredMSG { get; set; } = "Password Fiel Must Be Required..!!";
     }
 }
